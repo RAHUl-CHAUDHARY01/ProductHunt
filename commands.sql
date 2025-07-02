d88c92111b3eea5b98ede08bf473b648e4d01a20
gerryjindal <gerryjindal@gmail.com>
2025-07-02 22:36:23 +0530

    Changed the user schema and added the required columns

--- commands.sql
+++ commands.sql
@@ -1,10 +1,14 @@
--- //command to use this file :: mysql -u root -p producthunt < ./commands.sql
+-- Command to use this file: mysql -u root -p producthunt < ./commands.sql
 
-
-
-CREATE TABLE IF NOT EXISTS users(
+CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(150) NOT NULL UNIQUE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    password VARCHAR(255) NOT NULL,
+    github VARCHAR(255),
+    experience DECIMAL(3,1),
+    college VARCHAR(255) NOT NULL,
+    graduation_year YEAR NOT NULL,
+    location VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 );
